390f60a1c22e69bdd3692e9c1861ebce20de44b0
JaeMin <90915044+Min5107@users.noreply.github.com>
2024-04-02 23:21:00 +0900

    [11:20] String

---
+++ java-mid1/src/lang/immutable/address/ImmutableAddress.java
@@ -0,0 +1,20 @@
+package lang.immutable.address;
+
+public class ImmutableAddress {
+    private final String value;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public ImmutableAddress(String value) {
+        this.value = value;
+    }
+}
---
+++ java-mid1/src/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,18 @@
+package lang.string.method;
+
+public class StringSplitJoinMain {
+    public static void main(String[] args) {
+        String str = "Apple,Banana,Orange";
+
+        String[] split = str.split(",");
+        for (String s : split) {
+            System.out.println(s);
+        }
+
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println("연결된 문자열: " + joinedStr);
+
+        String result = String.join("-", split);
+        System.out.println("result = " + result);
+    }
+}
