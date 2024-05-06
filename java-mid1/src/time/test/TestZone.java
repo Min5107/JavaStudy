ede674784d7d5d81891a81a2cf2e020129f3ad59
JaeMin <90915044+Min5107@users.noreply.github.com>
2024-05-06 23:58:01 +0900

    [23:55] Nested

--- java-mid1/src/time/test/TestZone.java
+++ java-mid1/src/time/test/TestZone.java
@@ -10,7 +10,7 @@ public class TestZone {
         ZonedDateTime seoulTime = ZonedDateTime.of(LocalDate.of(2024, 1, 1), LocalTime.of(9,0), ZoneId.of("Asia/Seoul"));
         ZonedDateTime londonTime= ZonedDateTime.of(LocalDate.of(2024, 1, 1), LocalTime.of(9,0), ZoneId.of("Europe/London"));
         ZonedDateTime nyTime = ZonedDateTime.of(LocalDate.of(2024, 1, 1), LocalTime.of(9,0), ZoneId.of("America/New_York"));
-        
+
         System.out.println("서울의 회의 시간: " + seoulTime);
         System.out.println("런던의 회의 시간: " + londonTime);
         System.out.println("뉴욕의 회의 시간: " + nyTime);
