8f466c2555d3cbd4823aa8e949fdf7bf6850d881
JaeMin <90915044+Min5107@users.noreply.github.com>
2024-10-22 22:53:22 +0900

    [22:50] Thread

---
+++ java-adv1/src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,19 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+import static util.ThreadUtils.sleep;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedException implements Runnable{
+        @Override
+        public void run() /*throws Exception*/ {
+            // throw new Exception(); // 주석 풀면 예외 발생
+            sleep(1000);
+        }
+    }
+}
